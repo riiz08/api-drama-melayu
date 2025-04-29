bce4b8a4bc97f4723b8e653097d15e616f2587c0
riiz08 <mhmdrizki0812@gmail.com>
2025-04-29 10:45:02 +0700

    fix bug 11

--- dist/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/prisma.ts
-const prisma_1 = require("../src/generated/prisma");
-const globalForPrisma = globalThis;
-const prisma = globalForPrisma.prisma ?? new prisma_1.PrismaClient();
-if (process.env.NODE_ENV !== "production")
-    globalForPrisma.prisma = prisma;
-exports.default = prisma;
